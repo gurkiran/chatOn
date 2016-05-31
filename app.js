782315287c4f8b1c2afe891038c9bb606b4671c0
gurkiran <gurkiran4you@gmail.com>
2016-05-31 14:55:53 -0400

    minor fix

--- app.js
+++ app.js
@@ -27,7 +27,8 @@ io.on('connection',socket => {
   });
 });
 
-const port = 3000;
+
+const port = process.env.PORT || 3000;
 server.listen(port, () => {
   console.log(`server started on port ${port}`);
 });
