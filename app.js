3ff2f8036b0d291378fa3726c87ca8667c443230
gurkiran <gurkiran4you@gmail.com>
2016-06-14 22:07:42 -0400

    revamp styles

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-var express = require('express'),
-  http      = require('http');
-  socketIo  = require('socket.io');
+var express   = require('express'),
+    http      = require('http');
+    socketIo  = require('socket.io');
 
 var app = express();
+
+const server = new http.Server(app);
+const io = socketIo(server);
+
+const users = [];
+const connections =[];
 
 app.set('view engine','jade');
 
@@ -14,16 +20,38 @@ app.get('/', (req, res) => {
   res.render('index',{title :'Chat'});
 })
 
-const server = new http.Server(app);
-const io = socketIo(server);
 
-io.on('connection',socket => {
-  console.log('client connected');
-  socket.on('chat:add',data => {
+io.sockets.on('connection', (socket)=> {
+  connections.push(socket);
+  console.log('connnected: '+ connections.length);
+
+  socket.on('disconnect', (data)=> {
+    users.splice(users.indexOf(socket.username), 1);
+    updateUsernames();
+    connections.splice(connections.indexOf(socket), 1);
+    console.log('connnected: '+ connections.length);
+  })
+  // send message
+  socket.on('send message', (data) => {
     console.log(data);
-    io.emit('chat:added', data)
-  });
-});
+  io.sockets.emit('new message', {msg: data, user:socket.username});
+  })
+
+  socket.on('new message', (data, callback) => {
+    callback(true);
+    socket.username =data;
+    users.push(socket.username);
+    updateUsernames();
+  })
+
+  function updateUsernames() {
+    io.sockets.emit('get users', users)
+  }
+
+})
+
+
+
 
 
 const port = process.env.PORT || 3000;
--- public/app.js
+++ public/app.js
@@ -1,36 +1,45 @@
-const socket = io();
+$(function(){
 
-var person = prompt("Please enter your name");
+  var socket = io.connect();
 
+  var messageForm = $('#messageForm');
+  var message = $('#message');
+  var chat = $('#chat');
+  var userFormArea = $('#userFormArea');
+  var userForm = $('#userForm');
+  var messageArea = $('#messageArea');
+  var users = $('#users');
+  var username = $('#username');
 
-  const chatInput = document.querySelector('.chat-form input[type=text]');
-  chatInput.addEventListener('keypress', event => {
-    if (event.keyCode !== 13)
-      return;
+  messageForm.submit(function(e){
+    e.preventDefault();
+    console.log('submitted');
+    socket.emit('send message', message.val());
+    message.val('');
+  })
 
-    event.preventDefault();
+  socket.on('new message', function(data) {
+    chat.append('<div class="well"><strong>'+data.user+'</strong>: '+data.msg+'</div>');
+  })
 
-    const text = event.target.value.trim();
-    if (text.length === 0)
-      return;
-
-
-
-    socket.emit('chat:add', {
-      user: person,
-      message:text
+  userForm.submit(function(e){
+    e.preventDefault();
+    console.log('submitted');
+    socket.emit('new message', username.val(), function(data) {
+      if(data) {
+        userFormArea.hide();
+        messageArea.show();
+      }
     });
+    username.val('');
+  })
 
-    event.target.value ='';
+  socket.on('get users', function(data) {
+    var html = '';
+    for(var i=0; i < data.length; i++) {
+      html += '<li class="list-group-item">'+data[i]+'</li>';
+    }
+    users.html(html);
+  })
 
-  });
-
-
-
-const chatList = document.querySelector('.chat-list ul');
-socket.on('chat:added', data => {
-  const messageElement = document.createElement('li');
-  messageElement.innerText = data.user + ':' +data.message;
-  chatList.appendChild(messageElement);
-  chatList.scrollTop = chatList.scrollHeight;
 });
